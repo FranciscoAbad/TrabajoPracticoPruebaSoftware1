a2ed5c13c27cc88f3bf2e34ddd1b4d8ecb640d21
Facundo Elias Lopez <facueliaslopez@hotmail.com>
2023-06-20 16:37:37 -0300

    Se cambian variables a camel case y se mueven de carpeta los impl

---
+++ src/main/java/unla/oo2/grupo24/service/imp/BanioServiceImp.java
@@ -0,0 +1,45 @@
+package unla.oo2.grupo24.service.imp;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import unla.oo2.grupo24.entity.Banio;
+import unla.oo2.grupo24.repository.imp.BanioRepo;
+import unla.oo2.grupo24.service.IBanioService;
+
+
+@Service
+public class BanioServiceImp implements IBanioService {
+	
+	@Autowired
+	private BanioRepo repo;
+	
+	@Override
+	public List<Banio> listarTodos() {
+		
+		return repo.findAll();
+	}
+
+	@Override
+	public void guardarDispositivo(Banio b) {
+		repo.save(b);
+		
+	}
+
+	@Override
+	public Banio buscarId(long id) {
+		
+		return repo.findById(id).orElse(null);
+	}
+
+	@Override
+	public void eliminar(long id) {
+		repo.deleteById(id);
+		
+	}
+
+	
+
+}
