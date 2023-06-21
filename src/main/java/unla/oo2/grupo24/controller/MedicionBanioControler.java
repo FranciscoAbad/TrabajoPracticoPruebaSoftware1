b681fe2d911e1888897caac3ebf14fbada6b5a72
AlanCano123 <alancano123@example.com>
2023-06-21 16:51:55 -0300

    crear eventos

---
+++ src/main/java/unla/oo2/grupo24/controller/MedicionBanioControler.java
@@ -0,0 +1,48 @@
+package unla.oo2.grupo24.controller;
+
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import unla.oo2.grupo24.entity.Banio;
+import unla.oo2.grupo24.entity.Evento;
+import unla.oo2.grupo24.entity.MedicionBanio;
+import unla.oo2.grupo24.service.IBanioService;
+import unla.oo2.grupo24.service.imp.BanioServiceImp;
+import unla.oo2.grupo24.service.imp.MedicionBanioImp;
+
+@Controller
+public class MedicionBanioControler {
+	
+	@Autowired
+	IBanioService serviceMedicion;
+	 
+	 @GetMapping("/medicionesbanio")
+	    public String formRegistroEstacionamiento(Model model) {
+		 	
+	        return "views/dispositivos/medicionbanio";
+	    }
+	 
+	 @PostMapping("/medicionesbanio")
+	    public String agregarMedicionEstacionamiento(@RequestParam("fechaHora") LocalDateTime fechaHora, @RequestParam("idDispositivo") int idDispositivo,@RequestParam(value = "higienizado", defaultValue = "false") boolean higienizado) {
+	       
+	     Banio sensor=serviceMedicion.buscarId(idDispositivo);
+	      
+	       if(higienizado!=sensor.isHigienizado()){
+
+	          
+	            sensor.setHigienizado(higienizado);
+	            MedicionBanio medicion=new MedicionBanio(fechaHora,sensor,higienizado);
+	            serviceMedicion.guardarDispositivo(sensor);
+	     
+	        }
+
+	        return "views/dispositivos/medicionbanio";
+	    }
+
+}
---
+++ src/main/java/unla/oo2/grupo24/service/IMedicionBanioService.java
@@ -0,0 +1,8 @@
+package unla.oo2.grupo24.service;
+
+import unla.oo2.grupo24.entity.MedicionBanio;
+
+
+public interface IMedicionBanioService extends GenericService<MedicionBanio>{ 
+
+}
